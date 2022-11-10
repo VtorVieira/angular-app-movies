e92f8a54f48cc0ecb2409f4eda7a2174a475fb7c
vtorvieira <vieira.vitor42@gmail.com>
2022-11-10 17:13:33 -0300

    style: add new style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,14 +16,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { PopularComponent } from './components/pages/popular/popular.component';
 import { PlayingComponent } from './components/pages/playing/playing.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SearchComponent } from './components/pages/search/search.component';
 import { UpcommingComponent } from './components/pages/upcoming/upcoming.component';
-import { appReducer } from './store/store.state';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { DetailsComponent } from './components/pages/details/details.component';
 import { ReviewsComponent } from './components/pages/reviews/reviews.component';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { appReducer } from './store/store.state';
 
 @NgModule({
   declarations: [
@@ -37,10 +40,12 @@ import { ReviewsComponent } from './components/pages/reviews/reviews.component';
   ],
   imports: [
     FormsModule,
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
+    MdbCarouselModule,
     ReactiveFormsModule,
     StoreModule.forRoot({ app: appReducer }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
